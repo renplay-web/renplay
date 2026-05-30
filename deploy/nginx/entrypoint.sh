8bed2db6f503731c5456c463c48c9e610b8aefaf
janpuc <janpuc@proton.me>
2026-05-30 23:14:13 +0200

    chore: rewrite to not need root

--- deploy/nginx/entrypoint.sh
+++ deploy/nginx/entrypoint.sh
@@ -5,13 +5,13 @@ GAMES_DIR="${GAMES_DIR:-/games}"
 SAVES_DIR="${SAVES_DIR:-/data/saves}"
 DATA_DIR="${DATA_DIR:-/data}"
 
-mkdir -p "$GAMES_DIR" "$SAVES_DIR" "$DATA_DIR"
+mkdir -p "$GAMES_DIR" "$SAVES_DIR" "$DATA_DIR" /tmp/nginx
 
 export GAMES_DIR
 export SAVES_DIR
 export DATA_DIR
 
-envsubst '${GAMES_DIR} ${DATA_DIR}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${GAMES_DIR} ${DATA_DIR}' < /etc/nginx/nginx.conf.template > /tmp/nginx/nginx.conf
 
 node /app/save-sync/dist/index.js &
 SAVE_SYNC_PID=$!
@@ -25,4 +25,4 @@ fi
 trap 'kill $SAVE_SYNC_PID; exit' INT TERM
 
 echo "Starting nginx..."
-nginx -g 'daemon off;'
+nginx -c /tmp/nginx/nginx.conf -g 'daemon off;'
